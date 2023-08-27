4f86fa982957d52817e955eb3d17cf1a20741ce0
gNaboa <48036612+gNaboa@users.noreply.github.com>
2023-08-27 18:12:52 -0300

    tabs-update

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,11 +1,16 @@
 import { Stack, router, useLocalSearchParams } from 'expo-router'
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { ActivityIndicator, RefreshControl, SafeAreaView, ScrollView, Text, View } from 'react-native'
 import { COLORS, SIZES, icons } from '../../constants'
 import ScreenHeaderBtn from '../../components/common/header/ScreenHeaderBtn'
 import useFetch from '../../hooks/useFetch'
 import Company from '../../components/jobdetails/company/Company'
 import JobTabs from '../../components/jobdetails/tabs/Tabs'
+import Specifics from '../../components/jobdetails/specifics/Specifics'
+import JobAbout from '../../components/jobdetails/about/About'
+
+const tabs = ['About', 'Qualifications', 'Responsibilities']
+
 function JobDetails() {
 
     const params = useLocalSearchParams()
@@ -14,9 +19,29 @@ function JobDetails() {
     })
 
     const [refreshing, setRefreshing] = useState(false)
+    const [activeTab, setActiveTab] = useState(tabs[0])
 
     const onRefresh = () => {
 
+    }
+
+    const displayTabContent = () => {
+        switch (activeTab) {
+            case 'Qualifications':
+                return <Specifics
+                    title="Qualifications"
+                    points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+                />
+            case 'About':
+                return <JobAbout
+                    info={data[0].job_description ?? "No data provided"}
+                />
+            case 'Responsibilities':
+                return <Specifics
+                    title="Responsibilities"
+                    points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+                />
+        }
     }
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -59,14 +84,23 @@ function JobDetails() {
                     ) : (
                         <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
                             <Company
-                            companyLogo={data[0].employer_logo}
-                            jobTitle={data[0].job_title}
-                            companyName={data[0].employer_name}
-                            location={data[0].job_country}
+                                companyLogo={data[0].employer_logo}
+                                jobTitle={data[0].job_title}
+                                companyName={data[0].employer_name}
+                                location={data[0].job_country}
                             />
+
+                            <JobTabs
+                                tabs={tabs}
+                                activeTab={activeTab}
+                                setActiveTab={setActiveTab}
+                            />
+
+                            {displayTabContent()}
                         </View>
                     )}
                 </ScrollView>
+
             </>
 
         </SafeAreaView>
--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 
 import styles from './tabs.style'
 
-const Tabs = () => {
+import { SIZES } from '../../../constants'
+
+
+const TabButton = ({ name, activeTab, onHandleSearchType }) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <TouchableOpacity
+      style={styles.btn(name, activeTab)}
+      onPress={onHandleSearchType}
+    >
+      <Text style={styles.btnText(name,activeTab)}>{name}</Text>
+    </TouchableOpacity>
+  )
+
+}
+
+const JobTabs = ({ tabs, activeTab, setActiveTab }) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={item => item}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+      />
     </View>
   )
 }
 
-export default Tabs
+export default JobTabs
